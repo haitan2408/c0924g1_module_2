2a3373f5910ecbdcc86ee56f1a6c9d484f8bbb18
haitan2408 <haitan28102408@gmail.com>
2024-11-04 10:25:33 +0700

    up demo ss1

---
+++ src/ss1/Main.java
@@ -0,0 +1,17 @@
+package ss1;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 32768;
+        char c = 'c';
+        long d = 423434234;
+        int e = (int) d;
+//        Ép kiểu nguyên thủy implicit
+        System.out.println(e);
+    }
+
+
+}
