40ae5486e0be4b37389f23e8ef0614047d950b02
haitan2408 <haitan28102408@gmail.com>
2024-11-07 10:50:12 +0700

    up demo ss4

---
+++ src/ss4/Student.java
@@ -0,0 +1,22 @@
+package ss4;
+
+public class Student {
+    int code;
+    String name;
+    String address;
+    float point;
+
+    public Student(String address) {
+        this.address = address;
+        System.out.println("hi");
+    }
+    public Student() {
+        this("Vịet Nam");
+        System.out.println("hello");
+
+    }
+
+    public void example() {
+        System.out.println("Thi cử");
+    }
+}
